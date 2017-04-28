7714f8596598b83d3431291d50cde30fafbc0988
Dobachesky <mgdobachesky@email.neit.edu>
2017-04-28 13:53:14 -0400

    begin working on lab 4

---
+++ InClass/InClass4/src/Merlin.java
@@ -0,0 +1,18 @@
+
+public class Merlin {
+	private static Merlin theWizard;
+	
+	private Merlin() {}
+	
+	public static Merlin summon() {
+		if(theWizard == null) {
+			theWizard = new Merlin();
+		}
+		return theWizard;
+	}
+	
+	public String consult() {
+		return "Pull the sword from the stone.";
+	}
+}
+
